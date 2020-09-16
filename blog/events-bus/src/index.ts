75e9668395543d67c8fb4cdb4582b6bed87d4b62
Daute Rodríguez Rodríguez <dauterguezrguez@gmail.com>
2020-09-16 20:09:44 +0100

    Feat: basic event bus implementation

---
+++ blog/events-bus/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req: Request, res: Response) => {
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+
+  res.send({ status: 'ok' });
+});
+
+const port = 4005;
+app.listen(port, () => console.log(`Listening on ${port}`));
